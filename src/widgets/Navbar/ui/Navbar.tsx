ee1c2da2dac5e4c29fc6ffaac96396ea6a4b9a7e
LeshaD <alekseidarafeichyk@gmail.com>
2023-10-15 07:22:48 +0300

    feat: added Sidebar component

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -3,19 +3,14 @@ import cls from "./Navbar.module.scss";
 import { classNames } from "shared/lib/classNames/classNames";
 import { AppLink, AppLinkTheme } from "shared/ui/AppLink";
 import { useTheme } from "app/providers/ThemeProvider";
-import { ThemeSwitcher } from "widgets/ThemeSwitcher/ui/ThemeSwitcher";
 
 interface NavbarProps {
 	className?: string;
 }
 
 export const Navbar: FC<NavbarProps> = ({ className }) => {
-	const { toggleTheme } = useTheme();
-
 	return (
 		<div className={classNames(cls.navbar, {}, [className])}>
-			<ThemeSwitcher />
-
 			<div className={cls.links}>
 				<AppLink
 					className={cls.mainLink}
