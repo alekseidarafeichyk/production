bad27485b81eae749b1b6577a470efb871399357
LeshaD <alekseidarafeichyk@gmail.com>
2023-10-21 19:16:32 +0300

    feat: add eslint-react-i18n plugin

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,12 +5,14 @@ import { Navbar } from 'widgets/Navbar'
 import { Sidebar } from 'widgets/Sidebar'
 import { type FC, Suspense } from 'react'
 import './styles/index.scss'
+import { useTranslation } from 'react-i18next'
 
 export const App: FC = () => {
     const { theme } = useTheme()
+    const { t } = useTranslation()
 
     return (
-        <Suspense fallback={<div>Loading</div>}>
+        <Suspense fallback={<div>{t('Загрузка')}</div>}>
             <div className={classNames('app', {}, [theme])}>
                 <Navbar />
                 <div className="content-page">
