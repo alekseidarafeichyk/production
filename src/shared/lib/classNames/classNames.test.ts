198a91104b8ac492dddf513be7a49aa088fd160d
LeshaD <alekseidarafeichyk@gmail.com>
2023-10-22 10:41:39 +0300

    feat: add jest tests to project

---
+++ src/shared/lib/classNames/classNames.test.ts
@@ -0,0 +1,27 @@
+import { classNames } from './classNames'
+
+describe('classNames', () => {
+    test('with only first param', () => {
+        expect(classNames('mainClass')).toBe('mainClass')
+    })
+
+    test('with main and additional classes', () => {
+        expect(classNames('mainClass', {}, ['firstClass', 'secondClass'])).toBe('mainClass firstClass secondClass')
+    })
+
+    test('with mod', () => {
+        expect(classNames('mainClass', { hovered: true })).toBe('mainClass hovered')
+    })
+
+    test('with mods', () => {
+        expect(classNames('mainClass', { hovered: true, active: true })).toBe('mainClass hovered active')
+    })
+
+    test('with mods false', () => {
+        expect(classNames('mainClass', { hovered: false, active: true })).toBe('mainClass active')
+    })
+
+    test('with mods undefined', () => {
+        expect(classNames('mainClass', { hovered: undefined, active: true })).toBe('mainClass active')
+    })
+})
