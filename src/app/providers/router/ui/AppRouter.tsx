ac5b11a9a0e746a51a8d372f6c8b540934c2500e
LeshaD <alekseidarafeichyk@gmail.com>
2023-10-22 12:14:23 +0300

    feat: add PageLoader component

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,14 +1,12 @@
 import { type FC, Suspense } from 'react'
-import { useTranslation } from 'react-i18next'
 import { Route, Routes } from 'react-router-dom'
 import { routeConfig } from 'shared/config/routeConfig/routeConfig'
+import { PageLoader } from 'widgets/PageLoader'
 
 const AppRouter: FC = () => {
-    const { t } = useTranslation()
-
     return (
         <div className="page-wrapper">
-            <Suspense fallback={<div>{t('Загрузка')}</div>}>
+            <Suspense fallback={<PageLoader />}>
                 <Routes>
                     {Object.values(routeConfig).map(({ path, element }) => {
                         return <Route key={path} path={path} element={element} />
