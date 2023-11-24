4f7e8e1cc3937f8addf15977c265634296c2811b
LeshaD <alekseidarafeichyk@gmail.com>
2023-11-24 19:34:48 +0300

    fix: add renderFunction for unit tests

--- src/widgets/Sidebar/ui/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar.test.tsx
@@ -1,15 +1,15 @@
-import { fireEvent, render, screen } from '@testing-library/react'
-import { renderWithTranslation } from 'shared/lib/tests/renderWithTranslation'
+import { fireEvent, screen } from '@testing-library/react'
+import { componentRender } from 'shared/lib/tests/componentRender'
 import { Sidebar } from 'widgets/Sidebar'
 
 describe('Sidebar', () => {
     test('sidebar render', () => {
-        render(<Sidebar />)
+        componentRender(<Sidebar />)
         expect(screen.getByTestId('sidebar')).toBeInTheDocument()
     })
 
     test('test toggle', () => {
-        renderWithTranslation(<Sidebar />)
+        componentRender(<Sidebar />)
         const toggleButton = screen.getByTestId('sidebar-toggle')
         expect(screen.getByTestId('sidebar')).toBeInTheDocument()
         fireEvent.click(toggleButton)
